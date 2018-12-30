8fc07fce8d2b805f5db9421e431f0d32ffcfaa58
Pawan <pawan.km.m@gmail.com>
2018-12-30 18:33:00 +0530

    Added new SOP

--- MyTestProj/src/com/java/Test.java
+++ MyTestProj/src/com/java/Test.java
@@ -4,6 +4,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		System.out.println("Hello to Github");
+		System.out.println("Another SOP");
 
 	}
 	// ctrl+f11 to run the java project
