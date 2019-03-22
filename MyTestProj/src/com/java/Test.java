295fe59b6fbbdb1f0baa951e3cfe70fc3f52711d
Pawan <pawan.km.m@gmail.com>
2019-03-22 13:51:18 +0530

    Added comment for for loop

--- MyTestProj/src/com/java/Test.java
+++ MyTestProj/src/com/java/Test.java
@@ -5,6 +5,7 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("Hello to Github");
 		System.out.println("Another SOP");
+		// For loop added on 22 March 19
 		for(int i =1; i<=10;i++){
 			System.out.println("Printing i " +i);
 		}
