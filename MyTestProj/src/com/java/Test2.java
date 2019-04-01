2dc3418b92b7eeb092b5250fdf74c5be1e2298bb
Pawan <pawan.km.m@gmail.com>
2019-04-01 16:33:33 +0530

    Added new branch hotfix and added/modified new/existing files

--- MyTestProj/src/com/java/Test2.java
+++ MyTestProj/src/com/java/Test2.java
@@ -5,6 +5,16 @@ public class Test2 {
 	public static void main(String[] args) {
 		System.out.println("This is new file create to test github pull request functionality");
 	// ctrl+f11 to run the java project
+		
+		// Added the below line to fix the issue iss53 
+		//int number = 5;
+		for(int i=3; i<=5; i++){
+			System.out.println(i);
+			System.out.println(i+1);
+		}
+		
+		// add this comment in feature1
+		// add this comment in hotfix
 
 	}
 }
---
+++ MyTestProj/src/com/java/Test4.java
@@ -0,0 +1,11 @@
+package com.java;
+
+// Added as a part of hotfix
+public class Test4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
