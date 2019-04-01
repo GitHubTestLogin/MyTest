5c85da7d114b915672a2478e5f5b0ab41dde8386
Pawan <pawan.km.m@gmail.com>
2019-04-01 18:11:06 +0530

    added comment in Test2 file for feature1

--- MyTestProj/src/com/java/Test2.java
+++ MyTestProj/src/com/java/Test2.java
@@ -5,6 +5,8 @@ public class Test2 {
 	public static void main(String[] args) {
 		System.out.println("This is new file create to test github pull request functionality");
 	// ctrl+f11 to run the java project
+		
+		// Added this comment for feature1
 
 	}
 }
