090c96022429900fb8072e325a25d1f283724a59
Pawan <pawan.km.m@gmail.com>
2019-07-22 18:15:23 +0530

    modified for loop

--- MyTestProj/src/com/java/Test.java
+++ MyTestProj/src/com/java/Test.java
@@ -5,8 +5,8 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("Hello to Github");
 		System.out.println("Another SOP");
-		// For loop added on 22 March 19
-		for(int i =1; i<=10;i++){
+		// Modified for loop on 22 July March 19
+		for(int i =1; i<=5;i++){
 			System.out.println("Printing i " +i);
 		}
 
